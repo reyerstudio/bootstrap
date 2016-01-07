86ba670da13f088f1bdfcf6189ab43157ef4429e
Stéphane Este-Gracias <sestegra@gmail.com>
2016-01-07 17:29:23 +0100

    Add dart strapping

---
+++ strap/windows/dart.ps1
@@ -0,0 +1,15 @@
+#requires -v 3
+
+# Remote install
+# $ ra strap dart
+$erroractionpreference = 'stop' # quit if anything goes wrong
+
+# Install dart
+install_or_update dart
+install_or_update dartium
+install_or_update dartium_content_shell
+
+# Configure IDE
+which dart
+which dartium
+which dartium_content_shell
