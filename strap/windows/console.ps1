9f24f79cf0dab01ceca5be04cc4b86fd966e44ec
Stéphane Este-Gracias <sestegra@gmail.com>
2016-01-07 23:57:09 +0100

    Workaround for console

--- strap/windows/console.ps1
+++ strap/windows/console.ps1
@@ -7,4 +7,5 @@ $erroractionpreference = 'stop' # quit if anything goes wrong
 "Installing console environment..."
 install_or_update concfg
 sudo concfg import -n solarized small
+$erroractionpreference = 'SilentlyContinue' # workaround for pshazz
 install_or_update pshazz
