f96432c391a13a9c610717862add7393466d9eaf
Stéphane Este-Gracias <sestegra@gmail.com>
2016-01-09 15:48:19 +0100

    Update console.ps1

--- straps/windows/console.ps1
+++ straps/windows/console.ps1
@@ -1,11 +1,15 @@
 #requires -v 3
 
-# Remote install
-# $ ra strap console
 $erroractionpreference = 'stop' # quit if anything goes wrong
 
-"Installing console environment..."
-install_or_update concfg
-sudo concfg import -n solarized small
-$erroractionpreference = 'SilentlyContinue' # workaround for pshazz
-install_or_update pshazz
+function install() {
+  "Installing console environment..."
+  install_or_update concfg
+  sudo concfg import -n solarized small
+  $erroractionpreference = 'SilentlyContinue' # workaround for pshazz
+  install_or_update pshazz
+}
+
+function uninstall() {
+  "Uninstalling console environment..."
+}
