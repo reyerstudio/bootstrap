b5b3113929324dabe7cdcadeb660cba3997862ae
Stéphane Este-Gracias <sestegra@gmail.com>
2016-01-13 22:39:46 +0100

    ra 0.2.25

--- ra.rb
+++ ra.rb
@@ -1,12 +1,14 @@
 class Ra < Formula
   desc "Bootstrapping development environment"
-  version "0.3.0"
-  url "https://github.com/reyerstudio/devstrap/archive/1f79b827dbe57af163d93f5af8b6a18cb56e1614.zip"
-  sha256 "a0fe290c9eb510d3ac2231f53208dfcc8f595dc7e9e6a011a8e2585801d2e26e"
+  version "0.2.25"
+  url "https://github.com/reyerstudio/devstrap/archive/65a6ecb0b0073d83f8026a557b290743e7b96596.zip"
+  sha256 "9c0344f6536e54b4845f407a6e24d954f7579e9160cb56801be44ce8d1d30183"
 
   bottle :unneeded
 
   def install
-    bin.install "ra/unix/bin/ra"
+    bin.install     "ra/unix/bin/ra"
+    lib.install     Dir["ra/unix/lib/*"]
+    libexec.install Dir["ra/unix/libexec/*"]
   end
 end
