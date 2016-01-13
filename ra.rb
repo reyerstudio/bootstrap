ad5e0497b977d3f113fd563e3192baa8494818d7
Stéphane Este-Gracias <sestegra@gmail.com>
2016-01-13 22:44:58 +0100

    Increase minor release

--- ra.rb
+++ ra.rb
@@ -1,6 +1,6 @@
 class Ra < Formula
   desc "Bootstrapping development environment"
-  version "0.2.25"
+  version "0.3.0"
   url "https://github.com/reyerstudio/devstrap/archive/65a6ecb0b0073d83f8026a557b290743e7b96596.zip"
   sha256 "9c0344f6536e54b4845f407a6e24d954f7579e9160cb56801be44ce8d1d30183"
 
