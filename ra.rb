d78fcd5940fc89d02989c9fca3d293905456ce35
Stéphane Este-Gracias <sestegra@gmail.com>
2016-01-13 22:35:22 +0100

    ra 0.2.25

--- ra.rb
+++ ra.rb
@@ -1,8 +1,8 @@
 class Ra < Formula
   desc "Bootstrapping development environment"
-  version "0.2.24"
-  url "https://github.com/reyerstudio/devstrap/archive/42f152d81a2a2f49bd312c2c2eddb11b56cddc7c.zip"
-  sha256 "914a055720c76b7692bca0cba370aa73b2d36bbf2f4f6d31c257687557409146"
+  version "0.2.25"
+  url "https://github.com/reyerstudio/devstrap/archive/1f79b827dbe57af163d93f5af8b6a18cb56e1614.zip"
+  sha256 "a0fe290c9eb510d3ac2231f53208dfcc8f595dc7e9e6a011a8e2585801d2e26e"
 
   bottle :unneeded
 
