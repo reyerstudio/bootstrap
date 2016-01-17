fb19f3132be52a4180ebae37897402b32c283d19
Stéphane Este-Gracias <sestegra@gmail.com>
2016-01-17 11:51:19 +0100

    ra 0.3.8

--- ra.rb
+++ ra.rb
@@ -1,8 +1,8 @@
 class Ra < Formula
   desc "Bootstrapping development environment"
-  version "0.3.7"
-  url "https://github.com/reyerstudio/devstrap/archive/03b46992f5346631f031f304b5a61e812bf23b0f.zip"
-  sha256 "88d4193311d7a0da79dced451197d00b271564ea0be32bfcbaf33e49e55fccb3"
+  version "0.3.8"
+  url "https://github.com/reyerstudio/devstrap/archive/47c70787207664b361c2b7393049590ee3bb2f14.zip"
+  sha256 "b2b02ccfb134c1ea3507db3b736baf94e2a4117eb25ec0d695887b20c3c07167"
 
   bottle :unneeded
 
@@ -10,5 +10,6 @@ class Ra < Formula
     bin.install    "ra/unix/bin/ra"
     prefix.install Dir["ra/unix/func"]
     prefix.install Dir["ra/unix/libexec"]
+    prefix.install Dir["ra/unix/console"]
   end
 end
