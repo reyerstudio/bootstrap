6e7a58f7a32bb9ff0a0549f818631af4083b4d5e
Stéphane Este-Gracias <sestegra@gmail.com>
2017-07-06 19:58:02 +0200

    Add yes to Java PPA

--- straps/unix/core.sh
+++ straps/unix/core.sh
@@ -69,7 +69,7 @@ function core_linux() {
       # Enable HTTPS for apt
       sudo apt-get -y install apt-transport-https
       # Install Java 8
-      sudo add-apt-repository ppa:webupd8team/java
+      sudo add-apt-repository -y ppa:webupd8team/java
       sudo apt-get -y update
       echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
       sudo apt-get -y install oracle-java8-installer
